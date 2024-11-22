8afb2a6f0f0683faa7d9eb4e3be850d071ebd543
Angela-G9 <angelagathoni9568@gmail.com>
2024-11-22 14:49:05 +0300

    index .js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const name = "Susan";
 const height = 25;
-describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
-    });
-  });
-
-
 const message = `${name} is ${height} inches tall`;
 
 module.exports = { name, height, message };
 
-console.log( module.exports);
+console.log(message);
